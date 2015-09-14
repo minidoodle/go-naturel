c0c1c84891a378b32b6b0319be71280a81f35c09
Glen Tocher (SCE Antimidget) <sce.anti@gmail.com>
2015-09-14 20:20:32 +1200

    Added method to build an image of where it thinks skin is

--- naturel.go
+++ naturel.go
@@ -4,9 +4,10 @@ package naturel
 
 import (
 	"image"
+	"image/color"
 	_ "image/gif"
 	_ "image/jpeg"
-	_ "image/png"
+	"image/png"
 	"os"
 )
 
@@ -17,9 +18,9 @@ import (
 // Checks given file name for the skin ratio in the image
 // returning if we think it's porn and what our skinRaito
 // value came to
-func IsPorn(imgName string) (isPorn bool, skinRatio float64, err error) {
+func IsPorn(imgPath string) (isPorn bool, skinRatio float64, err error) {
 	// Read in our Image
-	reader, err := os.Open(imgName)
+	reader, err := os.Open(imgPath)
 	if err != nil {
 		return
 	}
@@ -49,4 +50,42 @@ func IsPorn(imgName string) (isPorn bool, skinRatio float64, err error) {
 		isPorn = true
 	}
 	return
+}
+
+// Outputs a PNG image with Red being where we thing skin is
+func HighlightSkin(imgPath string) error {
+	// Read in our Image
+	reader, err := os.Open(imgPath)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	// Decode the image
+	m, _, err := image.Decode(reader)
+	if err != nil {
+		return err
+	}
+	bounds := m.Bounds()
+	size := bounds.Size()
+
+	outImgFile, err := os.OpenFile(imgPath+"_skin.png", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+	defer outImgFile.Close()
+
+	outImg := image.NewNRGBA(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{size.X, size.Y}})
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, _ := m.At(x, y).RGBA()
+			// Look for a particular red color and variations of blue and green could reperesnt a skin color
+			if r > 60 && float64(g) < (float64(r)*0.85) && float64(b) < (float64(r)*0.7) && float64(g) > (float64(r)*0.4) && float64(b) > (float64(r)*0.2) {
+				outImg.SetNRGBA(x-bounds.Min.X, y-bounds.Min.Y, color.NRGBA{255, 0, 0, 255})
+			}
+		}
+	}
+
+	return png.Encode(outImgFile, outImg)
 }
